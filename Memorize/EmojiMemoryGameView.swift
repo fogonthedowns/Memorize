09c4d18bc2c1acf25f6cf165387f681afcb17605
Justin Zollars <fogonthedowns@gmail.com>
2021-07-10 12:41:55 -0700

    Lecture 7b

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -57,17 +57,27 @@ struct CardView: View {
         GeometryReader(content: { geometry in
             ZStack {
                 Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 110-90)).padding(DrawingConstants.circlePadding).opacity(DrawingConstants.circleOpacity)
-                Text(card.content).font(Font.system(size: min(geometry.size.width, geometry.size.height)*DrawingConstants.fontScale))
+                Text(card.content)
+                    .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+                    .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false)) // only effects modifiers above, not below
+                    .font(Font.system(size:DrawingConstants.fontSize))
+                    .scaleEffect(scale(thatFits:geometry.size))
             }
             .cardify(isFaceUp: card.isFaceUp)
         })
     }
+    
+    private func scale(thatFits size:CGSize) -> CGFloat {
+        min(size.width, size.height) / (DrawingConstants.fontSize/DrawingConstants.fontScale)
+    }
+    
     private struct DrawingConstants {
         static let cornerRadius: CGFloat = 10
         static let lineWidth: CGFloat = 2
-        static let fontScale: CGFloat = 0.65
+        static let fontScale: CGFloat = 0.7
         static let circlePadding: CGFloat = 4
         static let circleOpacity: Double = 0.5
+        static let fontSize: CGFloat = 32
     }
     
 }
